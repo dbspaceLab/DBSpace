16257ba207fa2812f9bc8707bbfa088e2b59eb5b
Vineet Tiruvadi <virati@gmail.com>
2018-03-03 13:17:29 -0500

    major changes from last time, forgot to commit every time there was a change.....

--- DBS_Osc.py
+++ DBS_Osc.py
@@ -14,6 +14,9 @@ import scipy.signal as sig
 
 import pdb
 
+import matplotlib.pyplot as plt
+plt.rcParams['image.cmap'] = 'jet'
+
 np.seterr(divide='raise')
 
 #Definitions for oscillatory features -> Oscillatory Vector
@@ -26,16 +29,88 @@ all_pts = ['901','903','905','906','907','908']
 def load_br_file(fname):
     rawdata = np.array(pd.read_csv(fname,sep=',',header=None))
     return rawdata
+
+def load_BR_dict(fname,sec_end=10):
+    txtdata = load_br_file(fname)[:,[0,2]]
     
-def gen_psd(inpX,Fs=422,nfft=2**10):
+    X = {'Left':txtdata[-(422*sec_end):-1,0],'Right':txtdata[-(422*sec_end):-1,1]}
+    
+    return X
+
+def gen_T(inpX,Fs=422,nfft=2**10):
+    outT = defaultdict(dict)
+    for chann in inpX.keys():
+        outT[chann] = {'T':np.linspace(0,inpX[chann].shape[0]/Fs,inpX[chann].shape[0]),'V':inpX[chann]}
+        
+    return outT
+        
+
+#ALERT    
+#gen_psd has gotten super complicated.... need to check if it still works with DSV and Regression stuff
+    
+def gen_psd(inpX,Fs=422,nfft=2**10,polyord=0):
     #inp X is going to be assumed to be a dictionary with different keys for different channels
     outPSD = defaultdict(dict)
+    outPoly = defaultdict(dict)
+    #assume input is time x seg
     for chann in inpX.keys():
         #The return here is a dictionary with two keys: F and PSD
-        outPSD[chann] = F_Domain(inpX[chann],Fs=Fs,nfft=nfft)['Pxx']
+        #check the size of the matrix now; it could be that we need to do this many times for each "segment"
+        fmatr = np.zeros((inpX[chann].shape[-1],int(nfft/2)+1))
+        polysub = np.zeros((inpX[chann].shape[-1],polyord+1))
+        
+        for seg in range(inpX[chann].shape[-1]):
+            psd = F_Domain(inpX[chann][:,seg].squeeze(),Fs=Fs,nfft=nfft)['Pxx']
+            
+            if polyord != 0:
+                intermed = 10*np.log10(psd)
+                fVect = np.linspace(0,Fs/2,int(nfft/2)+1)
+                
+                polyCoeff = np.polyfit(fVect,psd,polyord)
+                polyfunc = np.poly1d(polyCoeff)
+                polyitself = polyfunc(fVect)
+                
+                
+                fmatr[seg,:] = 10**((intermed - polyitself)/10)
+                polysub[seg,:] = polyCoeff
+                
+            else:
+                fmatr[seg,:] = psd
+            
+        
+        outPSD[chann] = fmatr.squeeze()
+        outPoly[chann] = polysub.squeeze()
 
     #Return here is a dictionary with Nchann keys
-    return outPSD
+    return outPSD,outPoly
+
+#This function takes a PSD and subtracts out the PSD's fourth order polynomial fit
+def poly_subtr(inpPSD,fVect,order=4):
+    #What's our feature/frequency vector?
+    
+    for chann in inpPSD.keys():
+        for seg in range(inpX[chann].shape[-1]):
+            polyCoeff = np.polyfit(fVect,inpPSD[chann][seg,:],order)
+            polyfunc = np.poly1d(polyCoeff)
+            polyitself = polyfunc(fVect)
+            
+            fix_psd[seg,:] = inpPSD[chann][seg,:] - polyfit
+            fix_poly[seg,:] = polyCoeff
+        
+        
+    
+    return fix_psd,fix_poly
+    
+
+def gen_SG(inpX,Fs=422,nfft=2**10,plot=False):
+    outSG = defaultdict(dict)
+    for chann in inpX.keys():
+        outSG[chann] = TF_Domain(inpX[chann])
+    
+    if plot:
+        plot_TF(outSG,chs=inpX.keys())
+    
+    return outSG
 
 def get_pow(Pxx,F,frange,cmode=np.median):
     #Pxx is a dictionary where the keys are the channels, the values are the [Pxx desired]
@@ -61,31 +136,48 @@ def get_pow(Pxx,F,frange,cmode=np.median):
     #return is going to be a dictionary with same elements
     return out_feats
 
+
+
 def F_Domain(timeser,nperseg=512,noverlap=128,nfft=2**10,Fs=422):
     #pdb.set_trace()
     #assert isinstance(timeser,dbs.timeseries)
     #Window size is about 1 second (512 samples is over 1 sec)
+    
+    #what are the dimensions of the timeser we're dealing with?
     
     Fvect,Pxx = sig.welch(timeser,Fs,window='blackmanharris',nperseg=nperseg,noverlap=noverlap,nfft=nfft)
     FreqReturn = {'F': Fvect,'Pxx': Pxx}
     
     return FreqReturn
 
-def TF_Domain(timeser,fs=422,noverlap=2**10-50):
+def TF_Domain(timeser,fs=422,nperseg=2**10,noverlap=2**10-50):
     #assert isinstance(timeser,dbs.timeseries)
-    
-    F,T,SG = sig.spectrogram(timeser,nperseg=2**10,noverlap=noverlap,window=sig.get_window('blackmanharris',2**10),fs=fs)
+    F,T,SG = sig.spectrogram(timeser,nperseg=nperseg,noverlap=noverlap,window=sig.get_window('blackmanharris',nperseg),fs=fs)
     
     TFreqReturn = {'T': T,'F':F,'SG': SG}
     
     return TFreqReturn
 
-def plot_TF(TFR,ch):
+def plot_TF(TFR,chs=['Left','Right']):
     plt.figure()
-    
-    plt.pcolormesh(TFR['T'],TFR['F'],10*(TFR['SG'][ch,:,:]))
-    plt.xlabel('Time')
-    plt.ylabel('Frequency')
+    for cc,chann in enumerate(chs):
+        plt.subplot(1,len(chs),cc+1)
+        aTFR = TFR[chann]
+        
+        plt.pcolormesh(aTFR['T'],aTFR['F'],10*np.log10(aTFR['SG']))
+        plt.xlabel('Time')
+        plt.ylabel('Frequency')
+        
+
+def plot_T(Tser):
+    plt.figure()
+    for cc,chann in enumerate(Tser.keys()):
+        plt.subplot(1,len(Tser.keys()),cc+1)
+        aT = Tser[chann]
+        plt.plot(aT['T'],aT['V'])
+        
+        plt.xlabel('Time')
+        
     
 ## BELOW THIS ARE THE STUDY RELATED FUNCTIONS/CLASSES
 all_phases = ['A0'+str(num) for num in range(4,0,-1)] + ['B0' + str(num) for num in range(1,5)] + ['C0' + str(num) for num in range(1,10)] + ['C' + str(num) for num in range(10,25)]
@@ -126,15 +218,6 @@ def get_slope(Pxx,F,params):
     #return is going to be a dictionary with same channel keys
     return out_feats
     
-
-## SIMPLE FUNCTIONS
-def unity(invar):
-    return invar
-
-
-def displog(values):
-    return 10*np.log10(values)
-
 #Variable definitions
 #Generalized Features
 #Need to regen this based off of the bands up there
@@ -146,12 +229,13 @@ feat_dict = {
                 'Gamma1':{'fn':get_pow,'param':(30,60)},
                 'Gamma2':{'fn':get_pow,'param':(60,100)},
                 'Stim':{'fn':get_pow,'param':(129,131)},
-                'SHarm':{'fn':get_pow,'param':(31,33)},
+                'SHarm':{'fn':get_pow,'param':(31,33)}, #Secondary Harmonic
+                'THarm':{'fn':get_pow,'param':(63,66)}, #Tertiary Harmonic
                 'Clock':{'fn':get_pow,'param':(104.5,106.5)},
                 'fSlope':{'fn':get_slope,'param':{'frange':(1,20),'linorder':1}},
                 'nFloor':{'fn':get_slope,'param':{'frange':(50,200),'linorder':0}}
             }
-feat_order = ['Delta','Theta','Alpha','Beta','Gamma1','fSlope','nFloor']
+feat_order = ['Delta','Theta','Alpha','Beta','Gamma1']#,'fSlope','nFloor']
 
 #Function to go through and find all the features from the PSD structure of dbo
 def calc_feats(psdIn,yvect):
@@ -173,6 +257,25 @@ def featDict_to_Matr(featDict):
     ret_matr = np.array([(featDict[feat]['Left'],featDict[feat]['Right']) for feat in feat_order])
     
     #assert that the size is as expected?
+    #should be number of feats x number of channels!
     assert ret_matr.shape == (len(feat_order),2)
     
     return ret_matr
+
+
+
+
+
+
+#%%
+## SIMPLE FUNCTIONS
+def unity(invar):
+    return invar
+
+
+def displog(values):
+    return 10*np.log10(values)
+
+
+def nestdict():
+    return defaultdict(nestdict)

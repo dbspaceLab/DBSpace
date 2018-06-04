0d329d287b42445231ff5f2e77a56a380f220e5c
Dolu Obatusin <mobatusin.tcsec@gmail.com>
2018-06-04 13:32:51 -0400

    working on subdiving files according to experiment

---
+++ pre_proc/rawtoexperiment.py
@@ -0,0 +1,20 @@
+"""
+The objective of this script will be to create an experiment folder with duplicate raw files renamed
+to actual experiments.
+
+
+Created on Mon June 4th 11:42AM 2018
+
+@author: Dolu Obatusin
+
+"""
+
+# import required modules / libraries
+
+# Note the location of the raw directories
+
+# Create new folder as experiment folder
+
+# Sort through each file in the folder
+
+# Create map for each folder

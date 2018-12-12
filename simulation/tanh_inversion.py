07b38c95a10acdcba2fd51cc485fe1edf8dfb8c9
Vineet Tiruvadi <vtiruva@emory.edu>
2018-12-12 17:20:26 -0500

    New additions to tanh_inversions

--- simulation/tanh_inversion.py
+++ simulation/tanh_inversion.py
@@ -16,6 +16,7 @@ import numpy as np
 
 def generator():
     sim_lfp = sig.detrend(brain_sig(1000,13,2e-7).ts_return(),type='constant')
+    
     return sim_lfp
 
 
@@ -25,8 +26,22 @@ gain = 1
 
 y_meas = gain * 5e-7 * np.tanh(x_true / 5e-7) + np.random.normal(0,2e-7,x_true.shape)
 
-x_pred = np.arctanh(y_meas / (np.max(np.abs(y_meas))+1e-9))
+#%%
 
-plt.plot(x_true)
-plt.plot(y_meas)
-plt.plot(x_pred)
+
+x_pred = 1e-6 * np.arctanh(y_meas / (np.max(np.abs(y_meas))+1e-9))
+
+plt.plot(x_true,label='True')
+plt.plot(y_meas,label='Measure')
+plt.plot(x_pred,label='Predicted',alpha=0.5)
+plt.legend()
+
+# Plot the PSDs now
+xs = [x_true, y_meas, x_pred]
+
+plt.figure()
+for xl,x in enumerate(xs):
+    f,Pxx = sig.welch(x,fs=422)
+    plt.plot(f,10*np.log10(Pxx),label=xl)
+    
+plt.legend()

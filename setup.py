cc6b18330ae375a041501e007bad5d559e3d790c
Vineet Tiruvadi <virati@gmail.com>
2022-01-31 12:29:17 -0800

    added setup.py with basics

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
-import sys
+from setuptools import setup
+
+setup(
+    name='DBSpace',
+    version='0.9.0',
+    author='Vineet Tiruvadi',
+    author_email='virati@gmail.com',
+    packages=['DBSpace', 'DBSpace.test'],
+    url='http://pypi.python.org/pypi/DBSpace/',
+    license='LICENSE.txt',
+    description='Multimodal, Model-based DBS Analysis',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "numpy",
+        "pytest",
+    ],
+)

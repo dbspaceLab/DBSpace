c5db74c122cba25f1727dd99b255ff9fec97027c
Vineet Tiruvadi <virati@gmail.com>
2022-02-01 09:59:39 -0800

    trying to get setup working for other repos

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     version='0.9.0',
     author='Vineet Tiruvadi',
     author_email='virati@gmail.com',
-    packages=['DBSpace', 'DBSpace.test'],
+    packages=['DBSpace'],
+    package_dir={'':'src'}
     url='http://pypi.python.org/pypi/DBSpace/',
     license='LICENSE.txt',
     description='Multimodal, Model-based DBS Analysis',
     long_description=open('README.md').read(),
     install_requires=[
         "numpy",
-        "pytest",
     ],
 )

01d3fc9e7e78c54d0a68f7f1494668de2f794b2a
Vineet Tiruvadi <virati@gmail.com>
2022-02-01 16:02:51 -0800

    got dbspace installed in editable mode inside this repo - big steps!

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='DBSpace',
-    version='0.9.0',
-    author='Vineet Tiruvadi',
-    author_email='virati@gmail.com',
-    packages=['DBSpace'],
-    package_dir={'':'src'}
-    url='http://pypi.python.org/pypi/DBSpace/',
-    license='LICENSE.txt',
-    description='Multimodal, Model-based DBS Analysis',
-    long_description=open('README.md').read(),
+    name="DBSpace",
+    version="0.9.0",
+    author="Vineet Tiruvadi",
+    author_email="virati@gmail.com",
+    packages=["dbspace"],
+    package_dir={"": "src"},
+    url="http://pypi.python.org/pypi/DBSpace/",
+    license="LICENSE.txt",
+    description="Multimodal, Model-based DBS Analysis",
+    long_description=open("README.md").read(),
     install_requires=[
+        "python==3.8.12",
         "numpy",
     ],
 )

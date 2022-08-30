79ea2ad095214e91f8b310db22897e3a3d609b43
Vineet Tiruvadi <virati@gmail.com>
2022-08-29 22:10:55 -0700

    upgraded version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="DBSpace",
-    version="0.1.1",
+    version="0.2.1",
     author="Vineet Tiruvadi",
     author_email="virati@gmail.com",
     packages=["dbspace"],
